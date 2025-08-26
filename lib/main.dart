d6a628e2b011320aa0b2ebb2c2cdae83dbd0c326
yasirkhoirul <yasirkhoirul9@gmail.com>
2025-08-26 12:08:26 +0700

    belajar terkair statefull dan stateless flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
+
 
 class MyApp extends StatelessWidget{
+  const MyApp({Key? key}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        title: "Flutter Demo",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Scaffold(
+          body: Center(
+            child: Statefullcontoh(parameterwidget: "Halo saya adalah yasir"),
+          ),
+        ),
+    );
+    throw UnimplementedError();
+  }
+}
+
+//state-nya tidak dapat berubah (immutable)
+class Heading extends StatelessWidget{
+  final String text;
+
+  const Heading({Key? key, required this.text}) :super(key: key);
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      title: "flutter Demo",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello World!"),
-        ),
-        body: Center(
-          child: Text("Hello World1"),
-        ),
-      ),
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      )
     );
   }
+}
+
+class Statefullcontoh extends StatefulWidget{
+
+  final String parameterwidget;
+
+  const Statefullcontoh({super.key,required this.parameterwidget});
+
+  @override
+  State<Statefullcontoh> createState()=>_Statefullcontoh();
+}
+
+class _Statefullcontoh extends State<Statefullcontoh>{
+    double _dataState = 16.0; // ini state dari Widget ContohStateful
+ 
+    @override
+    Widget build(BuildContext context){
+        // isi sebuah widget
+        return Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(widget.parameterwidget,style: TextStyle(fontSize: _dataState),),
+          ElevatedButton(onPressed: (){
+            setState(() {
+              _dataState = 32.0;
+            });
+          }, child: const Text("Perbesar"))
+        ]
+        );
+    }
 }
