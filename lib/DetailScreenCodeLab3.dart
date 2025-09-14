a0ab1b5493ab6135aa7690fbe8d6e4320f0c6be9
yasirkhoirul <yasirkhoirul9@gmail.com>
2025-09-14 13:09:08 +0700

    menambahkan detail web untuk codelab 3 dan sudah bisa berjalan 2 platform

--- lib/DetailScreenCodeLab3.dart
+++ lib/DetailScreenCodeLab3.dart
@@ -7,6 +7,170 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    return LayoutBuilder(
+      builder: (context, constrain) {
+        if (constrain.maxWidth < 1200) {
+          return DetailMobilePage(data: data);
+        } else {
+          return DetailWebPage(data: data);
+        }
+      },
+    );
+  }
+}
+
+// class untuk tampilan web
+class DetailWebPage extends StatefulWidget {
+  final Namatempat data;
+  
+  const DetailWebPage({super.key, required this.data});
+
+  @override
+  State<DetailWebPage> createState() => _DetailWebPageState();
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  final scrollcontroller =ScrollController();
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 16,horizontal: 64),
+          child: Center(
+            child: SizedBox(
+              width: 1200,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Wisata Bandung",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(5),
+                              child: Image.asset(
+                                widget.data.imageAsset,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const SizedBox(height: 12),
+                            Scrollbar(
+                              controller: scrollcontroller,
+                              child: Container(
+                                height: 150,
+                                padding: const EdgeInsets.only(bottom: 16),
+                                child: ListView.builder(
+                                  controller: scrollcontroller,
+                                  itemCount: widget.data.imageUrls.length,
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder: (context, index) {
+                                    return Padding(
+                                      padding: EdgeInsetsGeometry.all(3),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadiusGeometry.circular(
+                                          5,
+                                        ),
+                                        child: Image.network(widget.data.imageUrls[index]),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 12,),
+                      Expanded(
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                            
+                              children: [
+                                Text(
+                                  widget.data.name,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(fontWeight: FontWeight.bold,fontSize: 32),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Icon(Icons.access_time),
+                                            Text(widget.data.openTime),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 12,),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.calendar_month),
+                                            Text(widget.data.openDays),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 12,),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.monetization_on),
+                                            Text(widget.data.ticketPrice),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 12,),
+                                      ],
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.only(right: 12),
+                                      child: Icon(Icons.favorite),
+                                    )
+                                  ],
+                                ),
+                                Text(widget.data.description, textAlign: TextAlign.center,)
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    scrollcontroller.dispose();
+    super.dispose();
+  }
+}
+
+// class untuk tampilan mobile
+class DetailMobilePage extends StatelessWidget {
+  final Namatempat data;
+  const DetailMobilePage({super.key, required this.data});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -16,7 +180,7 @@ class DetailScreen extends StatelessWidget {
               Stack(
                 alignment: AlignmentGeometry.center,
                 children: [
-                  Image.asset(data.imageAsset,fit: BoxFit.cover,),
+                  Image.asset(data.imageAsset, fit: BoxFit.cover),
                   Positioned(
                     top: 20,
                     left: 20,
@@ -28,11 +192,7 @@ class DetailScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Positioned(
-                    top: 20,
-                    right: 20,
-                    child: Favorit(),
-                  )
+                  Positioned(top: 20, right: 20, child: Favorit()),
                 ],
               ),
               Row(
@@ -121,7 +281,7 @@ class _Favorit extends State<Favorit> {
           isfavorit = !isfavorit;
         });
       },
-      icon: Icon(isfavorit? Icons.favorite: Icons.favorite_border),
+      icon: Icon(isfavorit ? Icons.favorite : Icons.favorite_border),
     );
   }
 }
--- lib/mainCodeLab3.dart
+++ lib/mainCodeLab3.dart
@@ -33,56 +33,138 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(title: Text("Wisata Bandung")),
-      body: SafeArea(
-        child: ListView.builder(
-          itemCount: namatempatList.length,
-          itemBuilder: (context, index) {
-            final datalist = namatempatList[index];
-            return InkWell(
-              onTap: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return DetailScreen(data: datalist);
-                    },
+      appBar: AppBar(
+        title: Text("Wisata Bandung, size layar adalah ${size.width}"),
+      ),
+      body: LayoutBuilder(
+        builder: (context, BoxConstraints constraint) {
+          if (constraint.maxWidth < 600) {
+            return Namatempatlist();
+          } else if(constraint.maxWidth >=600 && constraint.maxWidth <= 1200) {
+            return Namatempatgrid(col: 4,);
+          }else{
+            return Namatempatgrid(col: 6);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class Namatempatlist extends StatelessWidget {
+  const Namatempatlist({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SafeArea(
+      child: ListView.builder(
+        itemCount: namatempatList.length,
+        itemBuilder: (context, index) {
+          final datalist = namatempatList[index];
+          return InkWell(
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return DetailScreen(data: datalist);
+                  },
+                ),
+              ),
+            },
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusGeometry.circular(16),
+              ),
+              elevation: 8,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: ClipRRect(
+                      borderRadius: BorderRadiusGeometry.only(
+                        topLeft: Radius.circular(16),
+                        bottomLeft: Radius.circular(16),
+                      ),
+                      child: Image.asset(fit: BoxFit.fill, datalist.imageAsset),
+                    ),
                   ),
-                ),
-              },
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusGeometry.circular(16),
-                ),
-                elevation: 8,
-                child: Row(
+                  Expanded(
+                    flex: 7,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(datalist.name),
+                          Text(datalist.location),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Namatempatgrid extends StatelessWidget {
+  final int col;
+  const Namatempatgrid({super.key, required this.col});
+  @override
+  Widget build(BuildContext context) {
+    final datalist = namatempatList;
+    // TODO: implement build
+    return SafeArea(
+      child: GridView.count(
+        crossAxisCount: col,
+        children: datalist.map((e) {
+          return InkWell(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return DetailScreen(data: e);
+                },
+              ),
+            ),
+            child: Card(
+              elevation: 12,
+              child: Container(
+                padding: EdgeInsets.all(6),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     Expanded(
-                      flex: 3,
-                      child: ClipRRect(borderRadius: BorderRadiusGeometry.only(
-                        topLeft: Radius.circular(16),
-                        bottomLeft: Radius.circular(16)
-                      ),child: Image.asset(fit: BoxFit.fill,datalist.imageAsset))
+                      flex: 7,
+                      child: Image.asset(e.imageAsset, fit: BoxFit.cover),
                     ),
                     Expanded(
-                      flex: 7,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text(datalist.name),
-                            Text(datalist.location),
-                          ],
-                        ),
+                      flex: 3,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            e.name,
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text(e.location),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
